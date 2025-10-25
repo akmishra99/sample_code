5dfdd6b37fb69adee2b20984b61fbd2f468922a3
Alok Kumar Mishra <akmishra_99@yahoo.com>
2025-10-24 23:04:57 -0700

    added the right file ,by mistake wrong version of file was uploaded

--- bits_flipped.c
+++ bits_flipped.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdint.h>
 /*
 ** file bits_flipped.c
 to compile use following command
@@ -15,42 +14,30 @@ in given hex number = AAAA total_number of bits flipped = 16, Please note AAAA i
 /*
 ** Author:- Alok Kumar Mishra
 ** e-mail:- akmishra_99@yahoo.com
-** Date Oct 24,2025
-** this program finds the number of bits flipped in a given number,it also prints 
-** count of number of zero bits and one bits ,also prints number of zero and 1 bits flipped
+** Date Oct 23,2025
+** this program finds the number of bits flipped in a given number
 */
-
-typedef struct bit_info {    
-    unsigned long given_number = 0;
-    uint8_t number_of_zero_bits = 0;
-    uint8_t number_of_one_bits = 0;
-    uint8_t zero_bits_flipped = 0;
-    uint8_t one_bits_flipped = 0;
-    uint8_t total_bits_flipped = 0 ;
-} t_bit_info;
-
-t_bit_info  count_bits_flipped_in_a_number(unsigned long  given_number)
+unsigned long  count_bits_flipped_in_a_number(unsigned long  given_number)
 {
     unsigned long int temp = given_number;
     unsigned long  int temp2 = given_number;
-    uint8_t number_of_zero_bits = 0;
-    uint8_t number_of_one_bits = 0;
-    uint8_t zero_bits_flipped =0;
-    uint8_t one_bits_flipped = 0;
-    uint8_t total_bits_flipped = 0;
-    bool previous_zero_bit = false;
-    bool previous_one_bit = false;
-    t_bit_info bit_info;
+    unsigned int number_of_zero_bits = 0;
+    unsigned int number_of_one_bits = 0;
+    int zero_bits_flipped =0;
+    int one_bits_flipped = 0;
+    int total_bits_flipped = 0;
+    int previous_zero_bit = 0;
+    int previous_one_bit = 0;
     for (int i = 0; i < sizeof(given_number) *8; i++)
     {   
-        temp = temp2 & 0x00000000000000000000000000000001;
+        temp = temp2 & 0x0000000000000001;
 
 
         if (temp) {
-            previous_one_bit = true;
+            previous_one_bit++;
             number_of_one_bits++;
             if (previous_zero_bit) {
-                previous_zero_bit = false;
+                previous_zero_bit = 0;
                 zero_bits_flipped++;
                 total_bits_flipped++;
 
@@ -58,10 +45,10 @@ t_bit_info  count_bits_flipped_in_a_number(unsigned long  given_number)
         }
         else
         {
-            previous_zero_bit = true;
+            previous_zero_bit++;
             number_of_zero_bits++;
             if (previous_one_bit) {
-                previous_one_bit = false;
+                previous_one_bit = 0;
                 one_bits_flipped++;
                 total_bits_flipped++;
             }
@@ -70,26 +57,10 @@ t_bit_info  count_bits_flipped_in_a_number(unsigned long  given_number)
         temp2 = temp2 >> 1;
 
     }
-    
-    bit_info.given_number = given_number;
-    bit_info.number_of_zero_bits = number_of_zero_bits;
-    bit_info.number_of_one_bits = number_of_one_bits;  
-    bit_info.zero_bits_flipped = zero_bits_flipped;
-    bit_info.one_bits_flipped = one_bits_flipped;
-    bit_info.total_bits_flipped = total_bits_flipped;   
-
-    return bit_info;
+    return total_bits_flipped;
 
 }
-void print_bit_info(t_bit_info bit_info)
-{
-    printf("\t\ttotal_number of bits flipped = %d\n",bit_info.total_bits_flipped   );
-    printf("\t\tnumber_of_zero_bits = %d\n",bit_info.number_of_zero_bits   );
-    printf("\t\tnumber_of_one_bits = %d\n",bit_info.number_of_one_bits   );
-    printf("\t\tzero_bits_flipped = %d\n",bit_info.zero_bits_flipped   );
-    printf("\t\tone_bits_flipped = %d\n",bit_info.one_bits_flipped   );
-    
-}    
+
 
 int main(int argc, char const **argv,char **envp)
 {
@@ -98,16 +69,11 @@ int main(int argc, char const **argv,char **envp)
     unsigned long c_number = 0x11 ; /* this is  decimal number 17*/
     unsigned long d_number = 0x101; /* this is decimal number 257 */
     unsigned long e_number = 0b1010101010101010; /* this is decimal number 43690 hex number AAAA */
-    printf("in given hex number = 0x%X\n",a_number);
-    print_bit_info(count_bits_flipped_in_a_number(a_number));
-    printf("in given hex number = 0x%X\n",b_number);
-    print_bit_info(count_bits_flipped_in_a_number(b_number));
-    printf("in given hex number = 0x%X\n",c_number);
-    print_bit_info(count_bits_flipped_in_a_number(c_number));
-    printf("in given hex number = 0x%X\n",d_number);
-    print_bit_info(count_bits_flipped_in_a_number(d_number));  
-    printf("in given hex number = 0x%X\n",e_number);
-    print_bit_info(count_bits_flipped_in_a_number(e_number));
+    printf("in given hex number = %X total_number of bits flipped = %d\n",a_number,count_bits_flipped_in_a_number(a_number));
+    printf("in given hex number = %X total_number of bits flipped = %d\n",b_number,count_bits_flipped_in_a_number(b_number));
+    printf("in given hex number = %X total_number of bits flipped = %d\n",c_number,count_bits_flipped_in_a_number(c_number));
+    printf("in given hex number = %X total_number of bits flipped = %d\n",d_number,count_bits_flipped_in_a_number(d_number));  
+    printf("in given hex number = %X total_number of bits flipped = %lu\n",e_number,count_bits_flipped_in_a_number(e_number));
     return 0;
     
 
