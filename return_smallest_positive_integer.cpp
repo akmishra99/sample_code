8e36c1d076bf328483d963e267fa5f383dd00581
Alok Kumar Mishra <akmishra_99@yahoo.com>
2026-07-16 11:59:11 -0700

    this program returns smallest positive integer other then zeo
    given an array of N integers, returns the smallest positive integer
    **  (greater than 0) that does not occur in A.

---
+++ return_smallest_positive_integer.cpp
@@ -0,0 +1,87 @@
+/*
+** given an array of N integers, returns the smallest positive integer
+**  (greater than 0) that does not occur in A.
+**  Please see sample run of the program at the end of this comment.
+** here is sample values of A for which the program is tested:
+** 1. A = {1, 3, 6, 4, 1, 2} => returns 5
+** 2. A = {1, 2, 3} => returns 4    
+** 3. A = {-1, -3} => returns 1
+** Author: Alok Kumar Mishra
+** e-mail: akmishra_99@yahoo.com
+** Thursday July 16,2026
+**
+C:\Users\akmis\scratch_july_16_2026>g++ -g -o return_smallest_positive_integer return_smallest_positive_integer.cpp
+** here is a sample run of the program:
+C:\Users\akmis\scratch_july_16_2026>.\return_smallest_positive_integer.exe
+5
+4
+1
+Result for test case 1: 5
+Result for test case 2: 4
+Result for test case 3: 1
+
+C:\Users\akmis\scratch_july_16_2026>
+*/
+
+
+
+// you can use includes, for example:
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution( vector<int> &A) {
+    vector<int> result;
+    
+    
+    sort(A.begin(), A.end());
+    for ( int i = 0; i < A.size(); i++) {
+
+        
+        
+        
+        if (std::find(A.begin(), A.end(), i) != A.end()) {
+
+			continue;
+        }
+        else {
+            result.push_back(i);
+            
+        }
+        
+    }
+    if ((result.size() == 1) && (result[0] == 0)) {
+        result.push_back(A.back() + 1);
+    }
+    return result;
+}
+int main(int argc, char* argv[], char* envp[]) {
+    vector<int> A[3] = {{1, 3, 6, 4, 1, 2}, {1, 2, 3}, {-1,-3}};
+    vector<int> result[3];
+    for (int j = 0; j < 3; j++) {
+         result[j]= solution(A[j]);
+        for (int i = 0; i < result[j].size(); i++) {
+            if (result[j][i] == 0) {
+                continue;
+            }
+            else
+                cout << result[j][i] << " ";
+        }
+        cout << endl;
+    }
+    for ( int j = 0; j < 3; j++) {
+        cout << "Result for test case " << j + 1 << ": ";
+        for (int i = 0; i < result[j].size(); i++) {
+            if (result[j][i] == 0) {
+                continue;
+            }
+            else
+                cout << result[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
